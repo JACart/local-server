e10d301a1a34909ddda578e424d3624c49964bd2
Matty Wolfson <wolfsomp@dukes.jmu.edu>
2021-03-02 17:25:08 -0500

    moved cart.json pointer

--- cartState.js
+++ cartState.js
@@ -125,5 +125,5 @@ module.exports.rosDisconnect = () => {
 }
 
 function writeState() {
-  fs.writeFileSync('../cart.json', JSON.stringify(cartState))
+  fs.writeFileSync('cart.json', JSON.stringify(cartState))
 }
