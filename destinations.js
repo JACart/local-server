d1fd55bc77eadce50c7ed05b325b50882c6955ed
munizsaj <munizsaj@dukes.jmu.edu>
2022-04-14 15:26:10 -0400

    Added more destinations to fill the new map

--- destinations.js
+++ destinations.js
@@ -1,38 +1,238 @@
 module.exports = {
+//Template
+/*
+  {ID}: {
+    latitude: {lat},
+    longitude: {long},
+    name: {name},       //String being displayed on UI
+    speech: {[x,y,z]},   //Different names for speech system to recognize
+  },
+*/
+
+  //East Campus
+  King: {
+    latitude: 38.434376, 
+    longitude: -78.864105,
+    name: "King Hall",
+    speech: ["King", "King Hall"],
+  },
+  UREC: {
+    latitude: 38.433973, 
+    longitude: -78.867666,
+    name: "UREC",
+    speech: ["UREC", "you wreck", "you rack", "Recreation", "Recreation Center","gym", "jim", "iraq"],
+  },
+  Jennings: {
+    latitude: 38.430663, 
+    longitude: -78.866104,
+    name: "Jennings Hall",
+    speech: ["Jennings", "Jenning", "Jennings Hall", "Jenning Hall", "Paul Jenning", "Paul Jennings", "Paul Jennings Hall", "Paul Jenning Hall", "getting", "Paul getting"]
+  },
+  Convo: {
+    latitude: 38.432885,
+    longitude: -78.868613,
+    name: "Convocation Center",
+    speech: ["Convocation Center", "Convo", "combo"],
+  },
+  EnGeo: {
+    latitude: 38.433859,
+    longitude: -78.862175,
+    name: "Engineering",
+    speech: ["home", "Engineering", "in geo", "ngl", "india"],
+  },
+  Shenandoah: {
+    latitude: 38.431766,
+    longitude: -78.862851,
+    name: "Shenandoah Hall",
+    speech: ["Shenandoah", "Shenandoah Hall", "shindo"]
+  },
+  EHall: {
+    latitude: 38.431662,
+    longitude: -78.860880,
+    name: "E-Hall",
+    speech: [""],
+  },
+
+
+
+  //Central Campus (Anything between highway and train track (ie village))
+  White: {
+    latitude: 38.435938, 
+    longitude: -78.866804,
+    name: "White Hall",
+    speech: ["White", "White Hall", "whitehall"],
+  },
+  Huffman: {
+    latitude: 38.436312, 
+    longitude: -78.869054,
+    name: "Huffman Hall",
+    speech: ["Huffman", "Huffman Hall", "huffman all"],
+  },
+  Village: {
+    latitude: 38.436514, 
+    longitude: -78.869590,
+    name: "Village",
+    speech: ["Village", "Village Area"],
+  },
+  Bookstore: {
+    latitude: 38.436154, 
+    longitude: -78.870122,
+    name: "Bookstore",
+    speech: ["Bookstore", "bookstores"],
+  },
+  Showker: {
+    latitude: 38.433298, 
+    longitude: -78.872345,
+    name: "Showker Hall",
+    speech: ["Showker", "Showker Hall", "shocker hall", "shocker", "shoulder"],
+  },
+  Hartman: {
+    latitude: 38.432691, 
+    longitude: -78.873119,
+    name: "Hartman Hall",
+    speech: ["Hartman", "Hartman Hall", "hartman all"],
+  },
+  Sonner: {
+    latitude: 38.432306, 
+    longitude: -78.873984,
+    name: "Sonner Hall",
+    speech: ["Sonner", "Sonner Hall", "sooner", "sooner", "sewn or hall"],
+  },
+  XLabs: {
+    latitude: 38.431618, 
+    longitude: -78.875885,
+    name: "X-Labs",
+    speech: ["x labs"],
+  },
+  
+
+  //Main Campus
+  Taylor: {
+    latitude: 38.437972, 
+    longitude: -78.869804,
+    name: "Taylor Hall",
+    speech: ["Taylor", "Taylor Hall", "Taylor Down", "Taylor Down Under", 'taylor\'s'],
+  },
+  Hillside: {
+    latitude: 38.438206, 
+    longitude: -78.868895,
+    name: "Hillside Hall",
+    speech: ["Hillside", "Hillside Hall"],
+  },
+  Bell: {
+    latitude: 38.438533, 
+    longitude: -78.867833,
+    name: "Bell Hall",
+    speech: ["Bell", "Bell Hall", "bellhop"],
+  },
+  Union: {
+    latitude: 38.437395, 
+    longitude: -78.870531,
+    name: "Madison Union",
+    speech: ["Union", "Madison Union"],
+  },
+  DHall: {
+    latitude: 38.437526, 
+    longitude: -78.871589,
+    name: "D-Hall",
+    speech: ["D hall", "deal", "dining hall"],
+  },
+  dukesdining: {
+    latitude: 38.438764, 
+    longitude: -78.870600,
+    name: "Dukes Dining",
+    speech: ["Duke", "Duke Dining", "Dukes", "Dukes Dining"]
+  },
+  Carrier: {
+    latitude: 38.439054, 
+    longitude: -78.872503,
+    name: "Carrier Library",
+    speech: ["Carrier", "Carrier Library"],
+  },
+  Wilson: {
+    latitude: 38.438261,
+    longitude: -78.873425,
+    name: "Wilson Hall",
+    speech: ["Wilson", "Wilson Hall", "wilson all"],
+  },
+  Quad: {
+    latitude: 38.439277, 
+    longitude: -78.874921,
+    name: "The Quad",
+    speech: ["Quad", "The Quad", "quiet"],
+  },
+  Harrison: {
+    latitude: 38.438527, 
+    longitude: -78.874467,
+    name: "Harrison Hall",
+    speech: ["Harrison", "Harrison Hall", "harrison all"],
+  },
+  hillcrest: {
+    latitude: 38.438257, 
+    longitude: -78.872069,
+    name: "Hillcrest House",
+    speech: ["hillcrest house", "hillcrest", "honors", "honors college"],
+  },
+  
+/*
+  //Original Testing
   home: {
     latitude: 38.433859,
     longitude: -78.862175,
+    name: "Home",
+    speech: ["Home"],
   },
+  */
   cafe: {
     latitude: 38.433406,
     longitude: -78.86157,
+    name: "Cafe",
+    speech: ["Cafe"],
   },
   clinic: {
     latitude: 38.433125,
     longitude: -78.860713,
+    name: "Clinic",
+    speech: ["Clinic"],
   },
   mall: {
     latitude: 38.432923,
     longitude: -78.860962,
+    name: "Mall",
+    speech: ["Mall"],
   },
   movie: {
     latitude: 38.432637,
     longitude: -78.861113,
+    name: "Movie",
+    speech: ["Movie"],
   },
+/*
   diner: {
     latitude: 38.431662,
     longitude: -78.860880,
+    name: "Diner",
+    speech: [""],
   },
+  */
   festival: {
     latitude: 38.433019,
     longitude: -78.859893,
+    name: "Festival",
+    speech: ["festival", "festi"],
   },
   gym: {
     latitude: 38.43352,
     longitude: -78.86257,
+    name: "gym",
+    speech: ["gym", "jim"],
   },
+/*
   chesapeake: {
     latitude: 38.43299,
     longitude: -78.86240,
+    name: "Chesapeake",
+    speech: ["chesapeake"],
   }
+*/
 }
